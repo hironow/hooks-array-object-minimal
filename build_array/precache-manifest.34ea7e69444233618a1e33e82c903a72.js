f99c12b05775b4ed02e1af4ba4197f9359f553d9
hironow <hironow365@gmail.com>
2019-08-23 00:48:52 +0900

    add build result (array annd object type)

---
+++ build_array/precache-manifest.34ea7e69444233618a1e33e82c903a72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3db1debdf40a4136bf168d4d6a6eb45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8281ce44293a7d07cace",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "fa1ca5c0fa6e73489a02",
+    "url": "/static/js/2.17ef70ae.chunk.js"
+  },
+  {
+    "revision": "8281ce44293a7d07cace",
+    "url": "/static/js/main.de032d45.chunk.js"
+  },
+  {
+    "revision": "36214820c76dfc61bc6b",
+    "url": "/static/js/runtime~main.f35c657d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ build_object/precache-manifest.19ff48e3811dedb034b2e5824d9d660c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fb479debe41e941bf6ea9603e4bba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9273a3414bce4a90782c",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "4bb6386753609a56492b",
+    "url": "/static/js/2.68fa0da8.chunk.js"
+  },
+  {
+    "revision": "9273a3414bce4a90782c",
+    "url": "/static/js/main.5f40577c.chunk.js"
+  },
+  {
+    "revision": "36214820c76dfc61bc6b",
+    "url": "/static/js/runtime~main.f35c657d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
